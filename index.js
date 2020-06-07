aedf7c51ec8a34714c2b63e9b5fe543bc441697f
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-06-07 10:51:51 +0100

    Implemented Function

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-
 class Clock {
-//type code here
+  //type code here
+  constructor(hours, minutes = 0) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.date = new Date();
+  }
+
+  toString() {
+    this.date.setHours(this.hours), this.date.setMinutes(this.minutes);
+    return String(this.date.toString().match(/\d\d:\d\d/g));
+  }
+
+  plus = (mins) => {
+    this.minutes += mins;
+    return this;
+  };
+
+  minus = (mins) => {
+    this.minutes -= mins;
+    return this;
+  };
+
+  equals = (newClock) => this.toString() === newClock.toString();
 }
 
-module.exports = Clock
+module.exports = Clock;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-
+ 
 
 const Clock = require('./index');
 
